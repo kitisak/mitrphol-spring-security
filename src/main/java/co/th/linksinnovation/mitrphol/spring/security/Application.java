6b76a514f237f1ebbe2e8c63aea0fcd91c467b02
Jirawong Wongdokpuang <Kong@Jirawongs-iMac.local>
2016-09-14 06:07:05 +0700

    extra

--- src/main/java/co/th/linksinnovation/mitrphol/spring/security/Application.java
+++ src/main/java/co/th/linksinnovation/mitrphol/spring/security/Application.java
@@ -7,6 +7,10 @@ package co.th.linksinnovation.mitrphol.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
@@ -17,5 +21,17 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsOrigin() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/secured").allowedOrigins("http://localhost:8000");
+            }
+            
+        };
     }
 }
--- src/main/java/co/th/linksinnovation/mitrphol/spring/security/controller/HelloController.java
+++ src/main/java/co/th/linksinnovation/mitrphol/spring/security/controller/HelloController.java
@@ -5,6 +5,10 @@
  */
 package co.th.linksinnovation.mitrphol.spring.security.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     
+    private Logger log = LoggerFactory.getLogger(HelloController.class);
+    
+    @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping("/api/hello")
     String get(){
+        log.warn("Test {}", "haha");
         return "hello, world";
     }
     
     @RequestMapping("/api/secured")
-    String getSecured(){
+    String getSecured(@AuthenticationPrincipal String user){
+        log.debug("Authen user {}",user);
         return "hello, world secured";
     }
 }
